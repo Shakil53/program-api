407de8d484204fb0d9bfa81e708d0e97fb9433f3
Shakil53 <shakilahmed.dcc@gmail.com>
2022-07-26 20:09:27 +0600

    uptodate

---
+++ app.js
@@ -0,0 +1,10 @@
+const loadquotes = () => {
+  fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayquote(data));
+}
+
+const displayquote = quote => {
+  const quoteElement = document.getElementById('quote');
+  quoteElement.innerText = quote.quote;
+}
